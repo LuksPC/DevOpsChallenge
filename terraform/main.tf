bc84d1e358902eda1d770d27bef0a7bc5dcc9ff5
Lucas Pereira Cavalcante <lukaspc003@gmail.com>
2025-05-20 15:22:14 -0300

    Create main.tf
    Criação da main do terraform

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "DevOpsChallenge"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} > ../ansible/host_ip.txt"
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Permitir HTTP e SSH"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Permitir todo o tráfego de saída"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
